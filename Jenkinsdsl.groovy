adca0ba96df4cdb0a3585ed96dbb5e0da16a41a2
aadi10894 <55064617+aadi10894@users.noreply.github.com>
2021-08-05 18:29:27 -0400

    Create Jenkinsdsl.groovy

---
+++ Jenkinsdsl.groovy
@@ -0,0 +1,18 @@
+job('NodeJS example') {
+    scm {
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('aadi10894')
+            node / gitConfigEmail('aadithyabharadwaj1994@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("npm install")
+    }
+}
